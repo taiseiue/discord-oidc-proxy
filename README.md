3a31d013a09aa2c94f2fd6cc26becd7c2e517456
taiseiue <taiseiue@wsnet.jp>
2025-05-29 11:26:00 +0900

    簡単に初期設定できるようにした

--- README.md
+++ README.md
@@ -9,12 +9,12 @@ DiscordのOAuth2認証でOIDCログインできるようにするくん。
 
 ### Usage
 
-1. `pnpx wrangler kv:namespace create "AUTH_KV"`を実行してKVを作成する
+1. `pnpx wrangler kv namespace create "AUTH_KV"`を実行してKVを作成する
 2. 出てきたIDで`wrangler.jsonc`の`<YOUR_KV_NAMESPACE_ID>`を置換する。
-3. 一度`pnpx deploy`する
+3. 一度`pnpm install && pnpm release`する
 4. 出てきたデプロイ先Url(`https://hoge.workers.dev`)で`wrangler.jsonc`の`https://<YOUR_WORKERS>.workers.dev`を置換する
 5. OIDCを使うアプリにクライアントIDがあれば`<OIDC_AUDIENCE>`を置換する。
 6. [Discord Developer Portal](https://discord.com/developers/applications)でApplicationを作成する
 7. OAuth2のClient IDとClient Secretを控える
-8. `deploy.sh`を実行し、指示に従ってClient IDとClient Secretを入力する
-9. 完成
+8. `setup.sh`を実行し、指示に従ってClient IDとClient Secretを入力する
+9. `pnpm release`して完成
--- setup.sh
+++ setup.sh
@@ -5,11 +5,13 @@ read DISCORD_CLIENT_ID
 echo -n DISCORD_CLIENT_SECRET:
 read DISCORD_CLIENT_SECRET
 
-pnpm install
-(cd keys && ssh-keygen -q -t rsa -b 2048 -f jwtRS256 -m PKCS8)
+echo '[1/2] Making certificates...'
+(cd keys && ssh-keygen -q -t rsa -b 2048 -f jwtRS256 -m PKCS8 && ssh-keygen -q -f jwtRS256 -e -m PKCS8 > jwtRS256.pub)
+
+echo '[2/2] Configuring...'
 cat keys/jwtRS256 | pnpx wrangler secret put JWT_PRIVATE_KEY
 cat keys/jwtRS256.pub | pnpx wrangler secret put JWT_PUBLIC_KEY
 echo $DISCORD_CLIENT_ID | pnpx wrangler secret put DISCORD_CLIENT_ID
-echo DISCORD_CLIENT_SECRET | pnpx wrangler secret put DISCORD_CLIENT_SECRET 
+echo $DISCORD_CLIENT_SECRET | pnpx wrangler secret put DISCORD_CLIENT_SECRET
 
-pnpx wrangler deploy
+echo '[SUCCESS] Setup complete!'
